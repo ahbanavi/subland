16e857dfafc00e4e0dd20534bcebf302869ad4e5
Amirhossein Banavi <ahbanavi@gmail.com>
2022-01-20 13:05:57 +0330

    fix(redirect): redirect to subscene if the url is not from upload channel

--- public/redirect.php
+++ public/redirect.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Support\Str;
 use SubLand\Utilities\Helpers;
+use SubLand\Utilities\Subscene;
 
 require_once '../bootstrap/boot.php';
+
 
 $telegramIPs = [
     // based on https://ipinfo.io/AS62041
@@ -47,17 +50,24 @@ $telegramIPs = [
 $ip_dec = (float) sprintf("%u", ip2long($_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR']));
 $isTelegram = false;
 foreach ($telegramIPs as $telegramIP) {
-    if (!$isTelegram) {
-        // Make sure the IP is from telegram.
-        if ($ip_dec >= $telegramIP['lower'] and $ip_dec <= $telegramIP['upper']) {
-            $isTelegram = true;
-        }
+    // Make sure the IP is from telegram.
+    if ($ip_dec >= $telegramIP['lower'] and $ip_dec <= $telegramIP['upper']) {
+        $isTelegram = true;
+        break;
     }
 }
+
 if (!$isTelegram) {
     header("Location: https://t.me/$_ENV[BOT_USER_NAME]");
 } else {
-    $array = explode('/', $_SERVER['REQUEST_URI']);
-    $postID = (int) base64_decode(Helpers::base64UrlDecode(end($array)));
-    header("Location: https://t.me/$_ENV[UPLOAD_CHANNEL]/$postID");
+    // redirect to subscene if the url is from subscene
+    if (Str::contains($_SERVER['REQUEST_URI'], Subscene::BASE_URL)) {
+        $url = Str::replaceFirst('/redirect.php/', '', $_SERVER['REQUEST_URI']);
+        header("Location: $url");
+    } else {
+        // otherwise, redirect to upload channel
+        $array = explode('/', $_SERVER['REQUEST_URI']);
+        $postID = (int) base64_decode(Helpers::base64UrlDecode(end($array)));
+        header("Location: https://t.me/$_ENV[UPLOAD_CHANNEL]/$postID");
+    }
 }
